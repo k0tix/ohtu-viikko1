7be5de37a2874c229a372a24c654a8b6712566c8
Otto Pirttimaa <otto.pirttimaa@gmail.com>
2019-11-08 01:39:04 +0200

    Add unnecessary lines

--- src/main/java/ohtu/main/Main.java
+++ src/main/java/ohtu/main/Main.java
@@ -5,22 +5,30 @@ import ohtu.ohtuvarasto.Varasto;
 public class Main {
 
     public static void main(String[] args) {
-        /**
-         * 
-         * 
-         *
-         * 
-         * 
-         * 
-         * 
-         * 
-         * 
-         * 
-         * 
-         * 
-         * 
-         * 
-         * 
-         */
+        System.out.println("Break");
+        System.out.println("Break");
+        System.out.println("Break");
+        System.out.println("Break");
+        System.out.println("Break");
+        System.out.println("Break");
+        System.out.println("Break");
+        System.out.println("Break");
+        System.out.println("Break");
+        System.out.println("Break");
+        System.out.println("Break");
+        System.out.println("Break");
+        System.out.println("Break");
+        System.out.println("Break");
+        System.out.println("Break");
+        System.out.println("Break");
+        System.out.println("Break");
+        System.out.println("Break");
+        System.out.println("Break");
+        System.out.println("Break");
+        System.out.println("Break");
+        System.out.println("Break");
+        System.out.println("Break");
+        System.out.println("Break");
+        System.out.println("Break");
     }
 }
